170a67a8c8eb9dbf62101d39475994fc3d0d60df
deshan68 <arundeshan@gmail.com>
2023-11-02 19:02:44 +0530

    first commit

---
+++ Photo-Picker/MultipleSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleSelectionView.swift
+//  Photo-Picker
+//
+//  Created by arun deshan on 2023-11-02.
+//
+
+import SwiftUI
+
+struct MultipleSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultipleSelectionView()
+}
---
+++ Photo-Picker/ProfileSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSelectionView.swift
+//  Photo-Picker
+//
+//  Created by arun deshan on 2023-11-02.
+//
+
+import SwiftUI
+
+struct ProfileSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileSelectionView()
+}
