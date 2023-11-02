25bba34cd3b8a27ce81487a15af09f09e69844a6
deshan68 <arundeshan@gmail.com>
2023-11-02 13:09:14 +0530

    Initial Commit

---
+++ Photo-Picker/Photo_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_PickerApp.swift
+//  Photo-Picker
+//
+//  Created by arun deshan on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct Photo_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
